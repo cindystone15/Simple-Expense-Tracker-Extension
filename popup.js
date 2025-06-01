e2b25b31cdfda17ecd69ab64aa80fc8cc6877f6b
Cindy Freestone <cindystone2154@gmail.com>
2025-06-01 19:33:44 +0200

    my changes

--- popup.js
+++ popup.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const divList = document.querySelector(".listHolder");
   const ul = divList.querySelector('ul');
 
+  loadEntries();
 
   addBtn.addEventListener('click', () => {
     const amount = parseFloat(amountInput.value);
@@ -33,16 +34,67 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
+  
+
+
+
+  function saveEntries(entries){
+    chrome.storage.local.set({expenses: entries}) .then(()=>{
+        console.log('entries set');
+      });
+  };
+
+  function loadEntries() {
+  chrome.storage.local.get(['expenses'], (result) => {
+    if (result.expenses) {
+      entries = result.expenses;
+      total = entries.reduce((sum, e) => sum + e.amount, 0);
+      totalDisplay.textContent = total.toFixed(2);
+      updateList();
+    } else {
+      console.log('No previous entries found.');
+    }
+  });
+}
+
+
   function updateList(){
     ul.innerHTML ='';
-    entries.forEach(entry=>{
+    entries.forEach((entry, index)=>{
       const li = document.createElement('li');
       li.textContent = `${entry.description}: ${entry.amount.toFixed(2)} PLN`;
+
+
+      const delBtn = document.createElement('button');
+      delBtn.textContent = 'Delete';
+      delBtn.style.marginLeft = '10px';
+      // delBtn.style.width = '40%';
+      // delBtn.style.padding = '5px'; 
+      delBtn.addEventListener('click',()=>{
+          entries.splice(index, 1);
+
+          total = entries.reduce((sum,e)=> sum +e.amount,0);
+          totalDisplay.textContent = total.toFixed(2);
+
+          saveEntries(entries);
+          updateList();
+      });
+
+
+      li.appendChild(delBtn);
       ul.appendChild(li);
     });
+    
+  }
+
+
+  function toggleDiv(divId){
+      const div = document.getElementById(divId);
+      div.style.display = (div.style.display === "none" || div.style.display === "") ? "block" : "none"; // remember same same as if else statement
   }
 
   allDataBtn.addEventListener('click', () =>{
     updateList();
+    toggleDiv('listHolder');
   });
 });
